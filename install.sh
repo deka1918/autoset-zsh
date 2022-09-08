e59793821e7dc678af03ac9be803bedd32e38ab9
deka1918 <82169960+deka1918@users.noreply.github.com>
2022-09-08 17:55:54 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#ZSH and zplug
+
+sudo apt install git zsh zplug -y
+sudo apt update && sudo apt dist-upgrade -y
+chsh -s /bin/zsh
+
+#powerlevel10k & config
+
+git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+#auto-suggestions
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+echo 'source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh' >>~/.zshrc
+
+echo 'source ~/autoset-zsh/.zsh_history' >>~/.zshrc
+
+exec zsh
+p10k configure
+
